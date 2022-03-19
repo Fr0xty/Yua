09322dcd6a32bb55e799360fd7018cee17b248dd
Fr0xty <ongjiae8@gmail.com>
2022-03-19 19:50:31 +0800

    added addsong command

--- src/commands/music/addsong.ts
+++ src/commands/music/addsong.ts
@@ -1,3 +1,4 @@
+import { QueryType } from 'discord-player';
 import { Message, MessageEmbed } from 'discord.js';
 import { BaseCommand } from 'yuna';
 import { Yuna } from '../../config.js';
@@ -12,7 +13,64 @@ class addsong implements BaseCommand {
         this.description = 'add song to server playlist.';
     }
 
-    async execute(msg: Message<boolean>, args: string[]) {}
+    async execute(msg: Message<boolean>, args: string[]) {
+        /**
+         * return if not setup
+         */
+        const isSetup = await checkSetup(msg.guildId!);
+        if (!isSetup) return await msg.reply({ embeds: [Yuna.notSetupYetEmbed] });
+
+        /**
+         * get guild document
+         */
+        const guildDocument = Yuna.database.collection('guilds').doc(msg.guildId);
+        const fetchedGuildDocument = await guildDocument.get();
+        const guildDocumentData = fetchedGuildDocument.data();
+
+        /**
+         * if guild has no songs (not playing), create queue
+         */
+        const queue = guildDocumentData.songs.length
+            ? Yuna.player.getQueue(msg.guild!)
+            : Yuna.player.createQueue(msg.guild!, {
+                  ytdlOptions: {
+                      quality: 'highest',
+                      filter: 'audioonly',
+                      highWaterMark: 1 << 25,
+                      dlChunkSize: 0,
+                  },
+                  leaveOnEnd: false,
+                  leaveOnStop: false,
+                  leaveOnEmpty: false,
+                  initialVolume: 50,
+              });
+
+        /**
+         * search and add to queue
+         */
+        const query = args.join(' ');
+        const searchResults = await Yuna.player.search(query, {
+            requestedBy: msg.member!,
+            searchEngine: QueryType.AUTO,
+        });
+        /**
+         * return if no result
+         */
+        if (!searchResults)
+            return await msg.reply(
+                'Sorry, the query provided did not have any results. Please try a different link / search keyword.'
+            );
+        /**
+         * add search result to queue && add to firebase guild songs array
+         */
+        if (query.includes('http')) {
+            queue.addTracks(searchResults.tracks);
+            guildDocument.update(guildDocumentData.songs.push(...searchResults.tracks.map((track) => track.url)));
+        } else {
+            queue.addTrack(searchResults.tracks[0]);
+            guildDocument.update(guildDocumentData.songs.push(searchResults.tracks[0].url));
+        }
+    }
 }
 
 export default new addsong();
