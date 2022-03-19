79e1dc6723ac6ed0d616ecb70ed4f1e10a9f043f
Fr0xty <ongjiae8@gmail.com>
2022-03-19 21:16:49 +0800

    added skip command

---
+++ src/commands/music/skip.ts
@@ -0,0 +1,30 @@
+import { Message } from 'discord.js';
+import { BaseCommand } from 'yuna';
+import { Yuna } from '../../config.js';
+import { checkSetup } from '../../utils.js';
+
+class skip implements BaseCommand {
+    name: string;
+    description: string;
+
+    constructor() {
+        this.name = 'skip';
+        this.description = 'skip current song.';
+    }
+
+    async execute(msg: Message<boolean>, args: string[]) {
+        /**
+         * return if not setup
+         */
+        const isSetup = await checkSetup(msg.guildId!);
+        if (!isSetup) return await msg.reply({ embeds: [Yuna.notSetupYetEmbed] });
+
+        const queue = Yuna.player.getQueue(msg.guild!);
+
+        if (!queue)
+            return await msg.reply('There is no song in serverplaylist, add songs using `yuna addsong <url/keywords>`');
+        queue.skip();
+    }
+}
+
+export default new skip();
