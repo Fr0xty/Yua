b7fdd3103d0a32f5a43182c1707849fa23ebadf7
Fr0xty <ongjiae8@gmail.com>
2022-03-20 15:24:43 +0800

    clearplaylist command added

--- src/commands/music/clearplaylist.ts
+++ src/commands/music/clearplaylist.ts
@@ -1,18 +1,20 @@
-import { GuildMember, Message, MessageActionRow, MessageButton, MessageEmbed, VoiceBasedChannel } from 'discord.js';
+import { Message, MessageEmbed } from 'discord.js';
 import { BaseCommand } from 'yuna';
 import { Yuna } from '../../config.js';
 import { checkSetup } from '../../utils.js';
 
-class changevc implements BaseCommand {
+class clearplaylist implements BaseCommand {
     name: string;
     description: string;
 
     constructor() {
-        this.name = 'changevc';
-        this.description = 'change server vc where I will play songs.';
+        this.name = 'clearplaylist';
+        this.description = 'clear the entire serverplaylist. (irreversible)';
     }
 
     async execute(msg: Message<boolean>, args: string[]) {
+        console.log('command called');
+
         /**
          * return if not setup
          */
@@ -27,7 +29,51 @@ class changevc implements BaseCommand {
             return await msg.reply(
                 'There is no song in serverplaylist, add songs using `yuna addsong <url/keywords>`.'
             );
+
+        /**
+         * confirmation
+         */
+        let embed = new MessageEmbed().setColor(Yuna.color).setTitle('⚠️ARE YOU SURE⚠️').setDescription(`
+This command will **CLEAR** __every song in the server playlist__ **FOREVER**!
+Your action is inreversable.
+
+Write "CONFIRM" to confirm your change.
+                 `);
+
+        const confirmationMsg = await msg.reply({ embeds: [embed] });
+        const filter = (m: Message) => m.content === 'CONFIRM' && m.author.id === msg.author.id;
+        try {
+            await msg.channel.awaitMessages({ filter, max: 1, time: 60_000, errors: ['time'] });
+        } catch {
+            try {
+                await confirmationMsg.delete();
+            } catch {}
+            return await msg.reply('Back to safety! If you decide to clear, do `yuna clearplaylist` again.');
+        }
+
+        /**
+         * confirmed, update to database
+         */
+        const guildDocument = await Yuna.database.collection('guilds').doc(msg.guildId).get();
+        const guildDocumentData = guildDocument.data();
+        guildDocumentData.songs = [];
+        await Yuna.database.collection('guilds').doc(msg.guildId).update(guildDocumentData);
+
+        /**
+         * stop playing
+         */
+        queue.destroy(true);
+
+        /**
+         * success embed
+         */
+        embed = new MessageEmbed()
+            .setColor(Yuna.color)
+            .setTitle('Cleared Successfully')
+            .setDescription('Server Playlist has been cleared \n do `yuna addsong <url>` to add new songs!')
+            .setTimestamp();
+        await msg.reply({ embeds: [embed] });
     }
 }
 
-export default new changevc();
+export default new clearplaylist();
